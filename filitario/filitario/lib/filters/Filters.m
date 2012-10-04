a71d66147a66ddf32006ca9b8777a5d703fa108b
justin <furiousjezz@gmail.com>
2012-10-04 15:28:13 +0530

    Added Nostagia Filter

--- filitario/filitario/lib/filters/Filters.m
+++ filitario/filitario/lib/filters/Filters.m
@@ -23,10 +23,39 @@
 
 -(CGImageRef) warmify   :(CIImage*)image
 {
-    CIImage *orginalImage = image;
+    CIImage     *orginalImage       = image;
+    CIFilter    *temperatureFilter  = [CIFilter filterWithName:@"CITemperatureAndTint"];
+    
+    [temperatureFilter setValue:orginalImage forKey:kCIInputImageKey];
+    [temperatureFilter setValue:[CIVector vectorWithX:6500 Y:500] forKey:@"inputNeutral"];
+    [temperatureFilter setValue:[CIVector vectorWithX:2500 Y:600] forKey:@"inputTargetNeutral"];
+    
+    CIImage     *outputImage  = [temperatureFilter valueForKey:kCIOutputImageKey];
+    
+    return [filterContext createCGImage:outputImage fromRect:[outputImage extent] ];
+    
 }
 -(CGImageRef) nostalgia :(CIImage*)image
 {
+    //Apply a monochrome filter to turn to greyscale and then a sepia filter over it
+    
+    CIImage     *orginalImage       =   image;
+    CIFilter    *monochromeFilter   =   [CIFilter filterWithName:@"CIColorMonochrome"];
+    
+    [monochromeFilter setValue:orginalImage forKey:kCIInputImageKey];
+    [monochromeFilter setValue:[CIColor colorWithRed:.3 green:.3 blue:.3 ]forKey:@"inputColor"];
+    [monochromeFilter setValue:[NSNumber numberWithFloat:.65] forKey:@"inputIntensity"];
+    
+    CIImage    *monochromeFilterOutputImage = [monochromeFilter valueForKey:kCIOutputImageKey];
+    
+    CIFilter   *sepiaFilter        = [CIFilter filterWithName:@"CISepiaTone"];
+    
+    [sepiaFilter setValue:monochromeFilterOutputImage forKey:kCIInputImageKey];
+    [sepiaFilter setValue:[NSNumber numberWithFloat:.4] forKey:@"inputIntensity"];
+    
+    CIImage    *outputImage        = [sepiaFilter valueForKey:kCIOutputImageKey];
+    
+    return [filterContext createCGImage:outputImage fromRect:[outputImage extent]];
     
 }
 -(CGImageRef) vintage   :(CIImage*)image
