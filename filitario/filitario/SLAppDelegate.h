df20accfe676731ac144fc70f4250a94688f376a
justin <furiousjezz@gmail.com>
2012-10-04 13:01:30 +0530

    intial commit

---
+++ filitario/filitario/SLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLAppDelegate.h
+//  filitario
+//
+//  Created by justin on 04/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
